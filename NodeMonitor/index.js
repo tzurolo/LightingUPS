373ec42b699756ecdfa6107c7577f9cd8b974f5d
Tony Zurolo <tzurolo@gmail.com>
2017-10-15 23:07:53 -0400

    Add files via upload

--- NodeMonitor/index.js
+++ NodeMonitor/index.js
@@ -13,17 +13,35 @@ var serialPort = new SerialPort("COM5", {
   baudRate: 300
 });
 
-var statusResQueue = [];
-var responseLine = '';
-var responseLineNum = 0;
-var readingStatus = false;
-var statusMessageDataJSON = [];
-var insertDevEventStmt;
+var serialPortRxLine = '';
+var serialPortRxLineNum = 0;
+var serialPortTimeoutTimer = null;
+var cmdQueue = [];
+var currentSWVer = null;
+var currentSettings = null;
+var currentTCalOffset = null;
 
- var lastMicrocontrollerCommand = '';
+// requests info from the attached unit by queueing commands
+function requestUnitInfo ()
+{
+    cmdQueue = [];
+    cmdQueue.push("ver");
+    cmdQueue.push("settings");
+    cmdQueue.push("get tcaloffset");
+    cmdQueue.push("status");
+    sendCommandToMicrocontroller(cmdQueue[0]);
+}
+
+function serialPortTimedOut ()
+{
+    console.log('Serial port timeout');
+    requestUnitInfo();
+}
+
  function sendCommandToMicrocontroller (cmd) {
-    lastMicrocontrollerCommand = cmd;
     serialPort.write(cmd + "\r");
+    // start timeout timer
+    serialPortTimeoutTimer = setTimeout(serialPortTimedOut, 3000);
  }
 
  function queryStatus (res) {
@@ -57,110 +75,130 @@ function convertTempToF (
 function processMicrocontrollerMessage (
     message)
 {
-    // console.log("microcontroller: '" + message + '"');
-    switch (message[0]) {
-        case 'U' :
-            console.log('got status string');
-            var tokens = message.trim().split(/\s+/);
-            var eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "BV", ' +
-                   '"value": "' + tokens[1] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "AC", ' +
-                   '"value": "' + tokens[2] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "C", ' +
-                   '"value": "' + tokens[3] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "Light", ' +
-                   '"value": "' + tokens[4] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "Motion", ' +
-                   '"value": "' + tokens[5] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "Temp", ' +
-                   '"value": "' + tokens[6] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            sendCommandToMicrocontroller('status');
-            break;
-        case 'V' :
-            console.log('sw version');
-            var eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "swver", ' +
-                   '"value": "' + message + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            sendCommandToMicrocontroller('settings');
-            break;
-        case '{' :
-            console.log('got settings');
-            var settings = JSON.parse(message);
-            var eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "ID", ' +
-                   '"value": "' + settings.ID + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "Manual", ' +
-                   '"value": "' + settings.Manual + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            console.log('end of status');
-            sendCommandToMicrocontroller('get tcaloffset');
-            //io.emit('controllerMessage', eventJSON);
-            break;
-        case 't' :
-            var tokens = message.trim().split(/\s+/);
-            console.log('temp cal offset ' + tokens[1]);
-            var eventJSON =
-                '{ "type": "event", "data": ' + '{' +
-                   '"name": "Tcal", ' +
-                   '"value": "' + tokens[1] + '"}' +
-                '}';
-            io.emit('controllerMessage', eventJSON);
-            sendCommandToMicrocontroller('status');
-            break;
-        case 'unrecognized' :
-            // command not recognized by microcontroller.
-            if (lastMicrocontrollerCommand.length > 0) {
-                // retry sending command
-                sendCommandToMicrocontroller(lastMicrocontrollerCommand);
-            }
-            break;
-        default :
-            if (readingStatus) { 
-            } else {
-                // not reading status
-            }
-            break;
+    clearTimeout(serialPortTimeoutTimer);
+    var gotExpectedResponse = true; // empty message is a valid response
+    if (message.length > 0) {
+        switch (message[0]) {
+            case 'U' :
+                console.log('got status string');
+                var tokens = message.trim().split(/\s+/);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "BV", ' +
+                       '"value": "' + tokens[1] + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "AC", ' +
+                       '"value": "' + tokens[2] + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "C", ' +
+                       '"value": "' + tokens[3] + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Light", ' +
+                       '"value": "' + tokens[4] + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Motion", ' +
+                       '"value": "' + tokens[5] + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Temp", ' +
+                       '"value": "' + tokens[6] + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                break;
+            case 'V' :
+                currentSWVer = message;
+                console.log('sw version: ' + currentSWVer);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "swver", ' +
+                       '"value": "' + currentSWVer + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                break;
+            case '{' :
+                console.log('got settings');
+                var currentSettings = JSON.parse(message);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "ID", ' +
+                       '"value": "' + currentSettings.ID + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Mode", ' +
+                       '"value": "' + currentSettings.Mode + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Dark", ' +
+                       '"value": "' + currentSettings.Dark + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Auto", ' +
+                       '"value": "' + currentSettings.Auto + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Manual", ' +
+                       '"value": "' + currentSettings.Manual + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                console.log('end of status');
+                //io.emit('controllerMessage', eventJSON);
+                break;
+            case 't' :
+                var tokens = message.trim().split(/\s+/);
+                currentTCalOffset = tokens[1];
+                console.log('temp cal offset ' + currentTCalOffset);
+                var eventJSON =
+                    '{ "type": "event", "data": ' + '{' +
+                       '"name": "Tcal", ' +
+                       '"value": "' + currentTCalOffset + '"}' +
+                    '}';
+                io.emit('controllerMessage', eventJSON);
+                break;
+            default :
+                gotExpectedResponse = false;
+                break;
+        }
+    }
+    if (gotExpectedResponse) {
+        cmdQueue.shift();
+    } else {
+        console.log('retrying ' + cmdQueue[0]);
+    }
+    if (cmdQueue.length == 0) {
+        cmdQueue.push('status');
+    }
+    if (cmdQueue.length > 0) {
+        sendCommandToMicrocontroller(cmdQueue[0]);
     }
 }
 
 io.on('connection', function(socket){
     console.log('got a connection.');
   socket.on('command', function(msg){
-      sendCommandToMicrocontroller(msg);
-      console.log('got command: ' + msg);
+      cmdQueue.push(msg);
+      console.log('got command from client: ' + msg);
   });
   socket.on('disconnect', function () {
     console.log('lost connection.');
@@ -177,14 +215,14 @@ serialPort.on("open", function () {
 	  case '\n' :
 	      break;
 	  case '\r' :
-	      if (responseLine.length > 0) {
-                  console.log(++responseLineNum + ': ' + responseLine);
-		  processMicrocontrollerMessage(responseLine);
-		  responseLine = '';
+	      if (serialPortRxLine.length > 0) {
+                  console.log(++serialPortRxLineNum + ': ' + serialPortRxLine);
+		  processMicrocontrollerMessage(serialPortRxLine);
+		  serialPortRxLine = '';
 	      }
 	      break;
 	  default:
-	      responseLine += dataStrChar;
+	      serialPortRxLine += dataStrChar;
 	  }
      }
     // console.log('data received: ' + data);
@@ -193,7 +231,7 @@ serialPort.on("open", function () {
     console.log('serial port closed');
   });
 
-  sendCommandToMicrocontroller('ver');
+  requestUnitInfo();
 });
 
 
