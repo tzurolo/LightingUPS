322eb416a2bd073d22db585dde484690933ff324
Tony Zurolo <tzurolo@gmail.com>
2017-11-01 22:54:35 -0400

    full cycle reflash, settings
    reflash integrates with monitoring
    ability to change settings
    supports V1.0 firmware

--- NodeMonitor/index.js
+++ NodeMonitor/index.js
@@ -25,10 +25,24 @@ var currentSWVer = null;
 var currentSettings = null;
 var currentTCalOffset = null;
 
+var savedSettings = null;
+var savedTCalOffset = null;
+
+var Mode = {
+    Monitoring : 'Monitoring',
+    Reprogramming : 'Reprogramming'
+};
+var currentMode = Mode.Monitoring;
+
+ function sendCommandToMicrocontroller (cmd) {
+    serialPort.write(cmd + "\r");
+    // start timeout timer
+    serialPortTimeoutTimer = setTimeout(serialPortTimedOut, 3000);
+ }
+
 // requests info from the attached unit by queueing commands
 function requestUnitInfo ()
 {
-    cmdQueue = [];
     cmdQueue.push("ver");
     cmdQueue.push("settings");
     cmdQueue.push("get tcaloffset");
@@ -38,28 +52,30 @@ function requestUnitInfo ()
 
 function serialPortTimedOut ()
 {
-    console.log('Serial port timeout');
-    serialPortRxLine = '';
-    requestUnitInfo();
-    if (currentSettings !== null) {
-        currentSWVer = null;
-        currentSettings = null;
-        currentTCalOffset = null;
-        sendSWVersionToClient(currentSWVer);
-        sendSettingsToClient(currentSettings);
-        sendTCalOffsetToClient(currentTCalOffset);
+    if (currentMode == Mode.Monitoring) {
+        console.log('Serial port timeout');
+        serialPortRxLine = '';
+        if ((cmdQueue.length == 0) ||
+            ((cmdQueue.length == 1) && (cmdQueue[0] == 'status'))) {
+            // we were only requesting status. request all info
+            cmdQueue = [];
+            requestUnitInfo();
+        } else if (cmdQueue.length > 0) {
+            // there are commands in the queue - retry sending
+            sendCommandToMicrocontroller(cmdQueue[0]);
+        }
+        if (currentSettings !== null) {
+            // clear out old settings
+            currentSWVer = null;
+            currentSettings = null;
+            currentTCalOffset = null;
+            // blank out UI fields
+            sendSWVersionToClient(currentSWVer);
+            sendSettingsToClient(currentSettings);
+            sendTCalOffsetToClient(currentTCalOffset);
+        }
     }
 }
-
- function sendCommandToMicrocontroller (cmd) {
-    serialPort.write(cmd + "\r");
-    // start timeout timer
-    serialPortTimeoutTimer = setTimeout(serialPortTimedOut, 3000);
- }
-
- function queryStatus (res) {
- }
- 
 
 //
 //  routes
@@ -85,124 +101,145 @@ function convertTempToF (
     return degF.toFixed(1) + '&#x2109';
 }
 
+function sendEventToUI (
+    eventName,
+    eventValue)
+{
+    var eventJSON =
+        '{ "type": "event", "data": ' + '{' +
+           '"name": "' + eventName + '", ' +
+           '"value": "' + eventValue + '"}' +
+        '}';
+    io.emit('controllerMessage', eventJSON);
+}
+
+function defaultSettings ()
+{
+    return {
+            "ID": "",
+            "Mode": "",
+            "Dark": "",
+            "Auto": "",
+            "Manual": ""
+        };
+}
+
 function sendSettingsToClient (
     settings)
 {
     if (settings == null) {
-        settings = {
-            "ID": "-",
-            "Mode": "-",
-            "Dark": "-",
-            "Auto": "-",
-            "Manual": "-"
-        };
+        settings = defaultSettings();
     }
-    var eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "ID", ' +
-            '"value": "' + settings.ID + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
-    eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "Mode", ' +
-            '"value": "' + settings.Mode + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
-    eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "Dark", ' +
-            '"value": "' + settings.Dark + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
-    eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "Auto", ' +
-            '"value": "' + settings.Auto + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
-    eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "Manual", ' +
-            '"value": "' + settings.Manual + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
+    sendEventToUI('ID', settings.ID);
+    sendEventToUI('Mode', settings.Mode);
+    sendEventToUI('Dark', settings.Dark);
+    sendEventToUI('Auto', settings.Auto);
+    sendEventToUI('Manual', settings.Manual);
 }
 
 function sendSWVersionToClient (
     swver)
 {
     if (swver == null) {
-        swver = '-';
+        swver = '';
     }
-    var eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "swver", ' +
-            '"value": "' + swver + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
+    sendEventToUI('swver', swver);
 }
 
 function sendTCalOffsetToClient (
     tcaloffset)
 {
     if (tcaloffset == null) {
-        tcaloffset = '-'
+        tcaloffset = '';
     }
-    var eventJSON =
-        '{ "type": "event", "data": ' + '{' +
-            '"name": "Tcal", ' +
-            '"value": "' + tcaloffset + '"}' +
-        '}';
-    io.emit('controllerMessage', eventJSON);
+    sendEventToUI('Tcaloffset', tcaloffset);
+}
+
+function commenceReprogramming ()
+{
+    // save current settings to restore after reprogramming
+    savedSettings = currentSettings;
+    savedTCalOffset = currentTCalOffset;
+
+    //  child process for re-flashing firmware with avrdude
+    var child = execFile('avrdude', [
+        '-p', 'attiny84',
+        '-P', 'COM4',
+        '-c', 'avrispv2',
+        '-e',
+        '-U', 'flash:w:C:\\files\\LightingUPS\\firmware\\default\\LightingUPS.hex:i'
+        ]);
+    child.stdin.setEncoding('utf-8');
+
+    child.stdout.on('data', function (data) {
+        console.log('child stdout: "' + data + '"');
+    });
+
+    child.stderr.on('data', function (data) {
+        var logmsg = '';
+        for (i = 0; i < data.length; ++i) {
+            var c = data.charCodeAt(i);
+            if ((c < 32) || (c > 126)) {
+                logmsg += ('(' + c + ')');
+            } else {
+                logmsg += data[i];
+            }
+        }
+        console.log('child stderr: "' + logmsg + '"');
+    });
+
+    child.on('close', function () {
+        console.log('child exited');
+
+        // reestablish communications with unit
+        serialPortRxLine = '';
+        cmdQueue.push("ver");
+
+        // restore saved settings
+        if (savedSettings !== null) {
+            cmdQueue.push("set id " + savedSettings.ID);
+            cmdQueue.push("set mode " + savedSettings.Mode);
+            cmdQueue.push("set dark " + savedSettings.Dark);
+            cmdQueue.push("set auto " + savedSettings.Auto);
+            cmdQueue.push("set manual " + savedSettings.Manual);
+        }
+        if (savedTCalOffset !== null) {
+            cmdQueue.push("set tcaloffset " + savedTCalOffset);
+        }
+
+        // read settings afresh from  unit
+        cmdQueue.push("settings");
+        cmdQueue.push("get tcaloffset");
+        cmdQueue.push("status");
+        currentMode = Mode.Monitoring;
+        sendCommandToMicrocontroller(cmdQueue[0]);
+    });
 }
 
 function processMicrocontrollerMessage (
     message)
 {
     clearTimeout(serialPortTimeoutTimer);
+    var responseIsComplete = true;  // false indicates multi-line response
     var gotExpectedResponse = true; // empty message is a valid response
     if (message.length > 0) {
         switch (message[0]) {
+            case 'O' :
+                // OK
+                break;
             case 'U' :
+                // response to status query
                 console.log('got status string');
                 var tokens = message.trim().split(/\s+/);
-                var eventJSON =
-                    '{ "type": "event", "data": ' + '{' +
-                       '"name": "BV", ' +
-                       '"value": "' + tokens[1] + '"}' +
-                    '}';
-                io.emit('controllerMessage', eventJSON);
-                eventJSON =
-                    '{ "type": "event", "data": ' + '{' +
-                       '"name": "AC", ' +
-                       '"value": "' + tokens[2] + '"}' +
-                    '}';
-                io.emit('controllerMessage', eventJSON);
-                eventJSON =
-                    '{ "type": "event", "data": ' + '{' +
-                       '"name": "C", ' +
-                       '"value": "' + tokens[3] + '"}' +
-                    '}';
-                io.emit('controllerMessage', eventJSON);
-                eventJSON =
-                    '{ "type": "event", "data": ' + '{' +
-                       '"name": "Light", ' +
-                       '"value": "' + tokens[4] + '"}' +
-                    '}';
-                io.emit('controllerMessage', eventJSON);
-                eventJSON =
-                    '{ "type": "event", "data": ' + '{' +
-                       '"name": "Motion", ' +
-                       '"value": "' + tokens[5] + '"}' +
-                    '}';
-                io.emit('controllerMessage', eventJSON);
-                eventJSON =
-                    '{ "type": "event", "data": ' + '{' +
-                       '"name": "Temp", ' +
-                       '"value": "' + tokens[6] + '"}' +
-                    '}';
-                io.emit('controllerMessage', eventJSON);
+                sendEventToUI('BV', tokens[1]);
+                sendEventToUI('AC', tokens[2]);
+                sendEventToUI('C', tokens[3]);
+                sendEventToUI('Light', tokens[4]);
+                sendEventToUI('Motion', tokens[5]);
+                if (tokens.length > 6) {
+                    // V1.0 firmware did not use temperature sensor
+                    sendEventToUI('Temp', tokens[6]);
+                }
                 break;
             case 'V' :
                 currentSWVer = message;
@@ -210,14 +247,41 @@ function processMicrocontrollerMessage (
                 sendSWVersionToClient(currentSWVer);
                 break;
             case '{' :
-                console.log('got settings');
-                if (message[message.length-1] == '}') {
-                    currentSettings = JSON.parse(message);
-                    sendSettingsToClient(currentSettings);
-                    console.log('end of status');
+                console.log('receiving settings');
+                if (message.length == 1) {
+                    // firmware V1.0 format settings
+                    currentSettings = defaultSettings();
+                    responseIsComplete = false;
                 } else {
-                    gotExpectedResponse = false;
+                    if (message[message.length-1] == '}') {
+                        currentSettings = JSON.parse(message);
+                        sendSettingsToClient(currentSettings);
+                        console.log('end of settings');
+                    } else {
+                        gotExpectedResponse = false;
+                    }
                 }
+                break;
+            case ' ' :
+                var tokens = message.trim().split(/\s+/);
+                if (tokens.length == 2) {
+                    // V1.0 format setting
+                    responseIsComplete = false;
+                    console.log('got V1.0 setting:' + message);
+                    switch (tokens[0]) {
+                        case 'ID:'     : currentSettings.ID     = tokens[1]; break;
+                        case 'Mode:'   : currentSettings.Mode   = tokens[1]; break;
+                        case 'Dark:'   : currentSettings.Dark   = tokens[1]; break;
+                        case 'Auto:'   : currentSettings.Auto   = tokens[1]; break;
+                        case 'Manual:' : currentSettings.Manual = tokens[1]; break;
+                        default        : console.log('unexpected setting name: "' + tokens[0] + '"');
+                    }
+                }
+                break;
+            case '}' :
+                // end of V1.0 format settings
+                console.log('end of settings');
+                sendSettingsToClient(currentSettings);
                 break;
             case 't' :
                 var tokens = message.trim().split(/\s+/);
@@ -225,21 +289,50 @@ function processMicrocontrollerMessage (
                 console.log('temp cal offset ' + currentTCalOffset);
                 sendTCalOffsetToClient(currentTCalOffset);
                 break;
+            case 'u' :
+                // unrecognized command
+                if (cmdQueue[0] == 'ver') {
+                    // probably a unit with V1.0 firmware
+                    console.log('ver unrecognized - assuming V1.0');
+                    currentSWVer = 'V1.0';
+                    sendSWVersionToClient(currentSWVer);
+                } else if (cmdQueue[0] == 'get tcaloffset') {
+                    // probably a unit with V1.0 firmware
+                    console.log('get tcaloffset unrecognized');
+                    currentTCalOffset = null;
+                } else {
+                    gotExpectedResponse = false;
+                }
+                break;
             default :
                 gotExpectedResponse = false;
                 break;
         }
     }
-    if (gotExpectedResponse) {
-        cmdQueue.shift();
-    } else {
-        console.log('retrying ' + cmdQueue[0]);
-    }
-    if (cmdQueue.length == 0) {
-        cmdQueue.push('status');
-    }
-    if (cmdQueue.length > 0) {
-        sendCommandToMicrocontroller(cmdQueue[0]);
+    if (responseIsComplete) {
+        if (gotExpectedResponse) {
+            cmdQueue.shift();
+        } else {
+            console.log('retrying ' + cmdQueue[0]);
+        }
+        if (cmdQueue.length == 0) {
+            // finished all queued commands.
+            // determine what to do next
+            if (currentMode == Mode.Monitoring) {
+                // we are in monitoring mode.
+                // request status
+                cmdQueue.push('status');
+            } else if (currentMode == Mode.Reprogramming) {
+                // we are in reprogramming mode.
+                // initiate reprogramming
+                console.log('commence reprogramming...');
+                clearTimeout(serialPortTimeoutTimer);
+                commenceReprogramming();
+            }
+        }
+        if (cmdQueue.length > 0) {
+            sendCommandToMicrocontroller(cmdQueue[0]);
+        }
     }
 }
 
@@ -253,38 +346,8 @@ io.on('connection', function(socket){
                 sendSettingsToClient(currentSettings);
                 sendTCalOffsetToClient(currentTCalOffset);
                 break;
-            case 'reflash' :
-                //  child process for re-flashing firmware with avrdude
-                var child = execFile('avrdude', [
-                    '-p', 'attiny84',
-                    '-P', 'COM4',
-                    '-c', 'avrispv2',
-                    '-e',
-                    '-U',
-                    'flash:w:C:\\files\\LightingUPS\\firmware\\default\\LightingUPS.hex:i' ]);
-                child.stdin.setEncoding('utf-8');
-
-                child.stdout.on('data', function (data) {
-                    console.log('child stdout: "' + data + '"');
-                });
-
-                child.stderr.on('data', function (data) {
-                    var logmsg = '';
-                    for (i = 0; i < data.length; ++i) {
-                        var c = data.charCodeAt(i);
-                        if ((c < 32) || (c > 126)) {
-                            logmsg += ('(' + c + ')');
-                        } else {
-                            logmsg += data[i];
-                        }
-                    }
-                    console.log('child stderr: "' + logmsg + '"');
-                });
-
-                child.on('close', function () {
-                    console.log('child exited');
-                });
-
+            case 'reprogram' :
+                currentMode = Mode.Reprogramming;
                 break;
             default :
                 console.log('unrecognized server command');
@@ -301,30 +364,34 @@ io.on('connection', function(socket){
 });
 
 serialPort.on("open", function () {
-  console.log('serial port open');
-  serialPort.on('data', function(data) {
-      var dataStr = data.toString();
-      for (i = 0; i < dataStr.length; i++) {
-	  var dataStrChar = dataStr[i];
-	  switch(dataStrChar) {
-	  case '\n' :
-	      break;
-	  case '\r' :
-	      if (serialPortRxLine.length > 0) {
-                  console.log(++serialPortRxLineNum + ': ' + serialPortRxLine);
-		  processMicrocontrollerMessage(serialPortRxLine);
-		  serialPortRxLine = '';
-	      }
-	      break;
-	  default:
-	      serialPortRxLine += dataStrChar;
-	  }
-     }
-    // console.log('data received: ' + data);
-  });
-  serialPort.on('close', function() {
+    console.log('serial port open');
+    serialPort.on('data', function(data) {
+        var dataStr = data.toString();
+        for (i = 0; i < dataStr.length; i++) {
+            var dataStrChar = dataStr[i];
+            switch(dataStrChar) {
+                case '\n' :
+                    break;
+                case '\r' :
+                    if (serialPortRxLine.length > 0) {
+                        console.log(++serialPortRxLineNum + ': ' + serialPortRxLine);
+                        processMicrocontrollerMessage(serialPortRxLine);
+                        serialPortRxLine = '';
+                    }
+                    break;
+                default:
+                    serialPortRxLine += dataStrChar;
+                    break;
+            }
+        }
+        // console.log('data received: ' + data);
+    }
+);
+
+serialPort.on('close', function() {
     console.log('serial port closed');
-  });
+  }
+);
 
   requestUnitInfo();
 });
